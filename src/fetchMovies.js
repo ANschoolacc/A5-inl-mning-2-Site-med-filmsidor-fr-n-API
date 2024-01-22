7f20f35463f1b90bb905205eecdb64218b9fef4c
ANschoolacc <145000303+ANschoolacc@users.noreply.github.com>
2024-01-22 21:12:57 +0100

    Fetch all movies and rendering
    Made a new js file with functions for fetching api then importing those functions to the main routing file.

---
+++ src/fetchMovies.js
@@ -0,0 +1,16 @@
+import fetch from "node-fetch";
+//Alla filmer: https://plankton-app-xhkom.ondigitalocean.app/api/movies 
+//Enstaka film: https://plankton-app-xhkom.ondigitalocean.app/api/movies/<id> 
+const api = 'https://plankton-app-xhkom.ondigitalocean.app/api';
+
+export async function allMovies(){
+  const res = await fetch(api + '/movies');
+  const movies = await res.json();
+  return movies.data
+}
+
+export async function specMovie(id){
+  const res = await fetch(api + '/movies' + id);
+  const movie = await res.json;
+  return movie.data;
+}
