d69beae917d22dab592b931494709c37b20b991b
ANschoolacc <145000303+ANschoolacc@users.noreply.github.com>
2024-01-25 13:29:46 +0100

    Made a not found page and test
    Made a page for "No movie found" when entering a url with /movies/id when id is not found. Also made a test for if no page is found

--- routes/pages.js
+++ routes/pages.js
@@ -3,22 +3,30 @@ const router = express.Router()
 import { allMovies, specMovie } from '../src/fetchMovies.js';
 import markDownConv from '../src/markConv.js';
 
-router.get('/', (req, res) =>{
-  res.render('index', {title: "Startsida"})
+router.get('/', (req, res) => {
+  res.render('index', { title: "Startsida" })
 })
 
-router.get('/movies', async (req, res) =>{
+router.get('/movies', async (req, res) => {
   const movieRender = await allMovies();
-  res.render('movies', {movieRender: movieRender, title: "Filmer", sectionTitle: "ALLA FILMER"})
+  res.render('movies', { movieRender: movieRender, title: "Filmer", sectionTitle: "ALLA FILMER" })
 })
 
 router.get('/about', (req, res) => {
-  res.render('aboutUs', {title: "Om oss"})
+  res.render('aboutUs', { title: "Om oss" })
 })
 
-router.get('/movies/:id', async (req, res) =>{
+router.get('/movies/:id', async (req, res) => {
   const movieRender = await specMovie(req.params.id);
-  res.render('movies', {movieRender: movieRender, title: movieRender.title, sectionTitle: "", markDownText: markDownConv(movieRender.intro)})
+  if (movieRender === 'Page not found') {
+    const errorText = 'Filmen kunde inte hittas!'
+    res.status(404)
+    res.render('movies', { movieRender: errorText, title: 'Page not found', sectionTitle: '' })
+  } else {
+    res.render('movies', { movieRender: movieRender, title: movieRender.title, sectionTitle: "", markDownText: markDownConv(movieRender.intro) })
+
+  }
+
 })
 
 export default router
